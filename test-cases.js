30b05ffe4956f38e373abe713863fcfbb7ebb5f9
sreeni-pando <sreenivaasan@pando.ai>
2023-09-20 08:02:11 +0530

    screenshots added

--- test-cases.js
+++ test-cases.js
@@ -5,7 +5,6 @@ describe('Testing positive Cases',() => {
     it('1. Every second Cron', (done) => {
         const expression = '* * * * * /user/bin';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(60);
         expect(response.hoursArr.length).to.equal(23);
         expect(response.dayOfMonthArr.length).to.equal(31);
@@ -17,7 +16,6 @@ describe('Testing positive Cases',() => {
     it('2. At 12:00 p.m. (noon) every day', (done) => {
         const expression = '0 12 * * * /user/bin';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(1);
         expect(response.hoursArr.length).to.equal(1);
         expect(response.dayOfMonthArr.length).to.equal(31);
@@ -30,7 +28,6 @@ describe('Testing positive Cases',() => {
         and then starting at 6 p.m. and ending at 6:55 p.m every day`, (done) => {
         const expression = '0/5 13,18 * * * /user/bin';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(12);
         expect(response.hoursArr.length).to.equal(2);
         expect(response.dayOfMonthArr.length).to.equal(31);
@@ -42,7 +39,6 @@ describe('Testing positive Cases',() => {
     it('4. Every minute starting at 1 p.m. and ending at 1:05 p.m., every day', (done) => {
         const expression = '0-5 13 * * * /user/bin';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(6);
         expect(response.hoursArr.length).to.equal(1);
         expect(response.dayOfMonthArr.length).to.equal(31);
@@ -56,7 +52,6 @@ describe('Testing negative Cases',() => {
     it('1. Command Not Specified', (done) => {
         const expression = '* * * * *';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(0);
         expect(response.hoursArr.length).to.equal(0);
         expect(response.dayOfMonthArr.length).to.equal(0);
@@ -69,7 +64,6 @@ describe('Testing negative Cases',() => {
     it('2. Cron Expression contaions only 4 fields', (done) => {
         const expression = '* * * * /user/bin';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(0);
         expect(response.hoursArr.length).to.equal(0);
         expect(response.dayOfMonthArr.length).to.equal(0);
@@ -82,7 +76,6 @@ describe('Testing negative Cases',() => {
     it(`3. Minutes Greater than 60`, (done) => {
         const expression = '62 * * * * /user/bin';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(0);
         expect(response.hoursArr.length).to.equal(0);
         expect(response.dayOfMonthArr.length).to.equal(0);
@@ -95,7 +88,6 @@ describe('Testing negative Cases',() => {
     it(`3. Hours Greater than 24`, (done) => {
         const expression = '* 25 * * * /user/bin';
         const response = new CronExpression(expression).parseExpression();
-        console.log(`response:${JSON.stringify(response)}`);
         expect(response.minuteArr.length).to.equal(0);
         expect(response.hoursArr.length).to.equal(0);
         expect(response.dayOfMonthArr.length).to.equal(0);
